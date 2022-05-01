d2167c25cc1d0b868bace94e5c988b0e16a56577
Ankeet Parikh <ankeet.parikh96@gmail.com>
2022-04-30 22:40:42 -0400

    add assignment problem solution

--- assignment.cpp
+++ assignment.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <concepts>
 #define f first
 #define s second
 #define pb push_back
@@ -6,58 +7,120 @@
 #define endl '\n'
 #define vi vector<int>
 #define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vld vector<ld>
+#define vvld vector<vld>
 #define pii pair<int, int>
+#define pll pair<ll, ll>
 #define vpii vector<pii>
+#define vvpii vector<vpii>
+#define vpll vector<pll>
+#define vvpll vector<vpll>
+#define rep(s, l, r) for (int s = l; s < r; s++)
+#define per(s, r, l) for (int s = r - 1; s >= l; s--)
+#define all(x) x.begin(), x.end()
+#define PI acosl(-1) 
 typedef long long ll;
 typedef long double ld;
 using namespace std;
+template<class T> using maxheap = priority_queue<T, vector<T>, less<T>>;
 template<class T> using minheap = priority_queue<T, vector<T>, greater<T>>;
-
+template<typename T> void setmax(T& a, T b) { a = max(a, b); };
+template<typename T> void setmin(T& a, T b) { a = min(a, b); };
+template<typename T> bool in(T lo, T v, T hi) { return lo <= v && v < hi; };
 /*
   Uses the Hungarian algorithm to solve the Assignment problem.
-  Note: The assignment problem is to find the MAXIMUM total
-  sum. If you want min, then turn each weight w into 
-  MAX_WEIGHT - w.
+  Finds the MINIMUM assignment of n workers to m tasks. Matching
+  at most min(n, m) workers to tasks.
 
-  Input: Square matrix of side length N with non-negative entries.
-  Output: Array [c_0, c_1, ..., c_{N-1}] where the assignments
-  are (0, c_0), (1, c_1), ..., (N - 1, c_{N - 1})
+  For MAXIMUM, multiply all weights by -1
 
-  Complexity: O(N ^ 3)
+  Complexity: O(n^2 * m)
 
-  https://www.cc.gatech.edu/~rpeng/18434_S15/hungarianAlgorithm.pdf
+  http://zafar.cc/2017/7/19/hungarian-algorithm/
 */
 
-int n, k; // bipartitions
-vvi g;
-vi mt;
-vi used;
+struct Assignment {
+  const ll INF = 1e18;
 
-bool try_kuhn(int v) {
-  if (used[v]) return false;
-  used[v] = true;
-  for (int y : g[v]) {
-    if (mt[y] == -1 || try_kuhn(mt[y])) {
-      mt[y] = v;
-      return true;
-    }
-  }
-  return false;
-}
+  vpll pairings;
+  ll mincost;
 
-void solve() {
-  mt.assign(k, -1);
-  for (int v = 0; v < n; v++) {
-    assert(mt[v] == -1); // can remove this assertion, just notice that it evals to true!
-    used.assign(n, 0);
-    try_kuhn(v);
-  }
-  for (int i = 0; i < k; i++) {
-    if (mt[i] != -1) {
-      printf("%d %d\n", mt[i] + 1, i + 1);
+  void solve(vvl _a) { // _a is zero-indexed and rectangular
+    int n = _a.size();
+    int m = _a[0].size();
+
+    vvl a(n + 1, vl(m + 1, 0));
+    for (int i = 1; i <= n; i++) {
+      copy(_a[i - 1].begin(), _a[i - 1].end(), a[i].begin() + 1);
     }
+
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= m; j++) {
+        cout << a[i][j] << " \n"[j == m];
+      }
+    }
+
+    vl u(n + 1);
+    vl v(m + 1), p(m + 1), way(m + 1);
+
+    for (int i = 1; i <= n; i++) {
+      p[0] = i;
+      ll j0 = 0;
+      vl minv(m + 1, INF);
+      vl used(m + 1, false);
+      do {
+        used[j0] = true;
+        ll i0 = p[j0], delta = INF, j1;
+        for (int j = 1; j <= m; j++) {
+          if (!used[j]) {
+            ll cur = a[i0][j] - u[i0] - v[j];
+            if (cur < minv[j]) {
+              minv[j] = cur, way[j] = j0;
+            } 
+            if (minv[j] < delta) {
+              delta = minv[j], j1 = j;
+            }
+          }
+        }
+        for (int j = 0; j <= m; j++) {
+          if (used[j]) {
+            u[p[j]] += delta, v[j] -= delta;
+          } else {
+            minv[j] -= delta;
+          }
+        }
+        j0 = j1;
+      } while (p[j0] != 0);
+      do {
+        ll j1 = way[j0];
+        p[j0] = p[j1];
+        j0 = j1;
+      } while (j0);
+    }
+
+    pairings.clear();
+    for (int i = 1; i <= m; i++) {
+      pairings.pb({p[i] - 1, i - 1});
+    }
+    mincost = -v[0];
   }
-}
+
+};
+
+
 int main() {
+
+  Assignment A;
+
+  vvl a = {{1, 2}, {1, 4}};
+
+  A.solve(a);
+
+  cout << A.mincost << endl;
+
   return 0;
 }
